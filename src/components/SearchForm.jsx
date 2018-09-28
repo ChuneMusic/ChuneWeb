f564b052fc18c9efd1ec2c6d5e850e4458602f77
Studenov Roman <rstudenov@allmax.team>
2018-09-28 15:49:56 +0700

    fixed bugs SearchForm: when press up or down (arrow) button there was an application error - done

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -4,10 +4,15 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import Autosuggest from 'react-autosuggest';
 import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
 import CloseIcon from '@material-ui/icons/Close';
+import {
+  func, objectOf, any,
+  arrayOf
+} from 'prop-types';
 
 import { searchArtists, clearListSearch } from '../store/autosuggest/actions';
+
+import '../styles/autosuggestion.css';
 
 const styles = () => ({
   root: {
@@ -16,100 +21,6 @@ const styles = () => ({
     boxSizing: 'border-box',
     backgroundColor: 'white',
     opacity: 1,
-  },
-  input: {
-    paddingLeft: 40,
-    margin: '5px auto 0 auto',
-    height: 69,
-    width: '99%',
-    display: 'block',
-    border: 'none',
-    transition: 'all 2s',
-    backgroundColor: '#ffffff',
-    fontSize: 24,
-    boxShadow: '0 0 4px 2px rgba(0, 0, 0, 0.25)',
-    boxSizing: 'border-box',
-    '&::placeholder': {
-      fontFamily: 'Roboto',
-      fontSize: 24,
-      fontWeight: 'normal',
-      fontStyle: 'normal',
-      fontStretch: 'normal',
-      lineHeight: 'normal',
-      letterSpacing: 0.3,
-      color: '#757575',
-    },
-    '&:focus': {
-      border: 'none',
-      outline: 'none',
-    },
-    '@media (max-width: 1023px)': {
-      height: 36,
-      paddingLeft: 16,
-      fontSize: 36,
-      '&::placeholder': {
-        fontFamily: 'Roboto',
-        fontSize: 36,
-        fontWeight: 'normal',
-        fontStyle: 'normal',
-        fontStretch: 'normal',
-        lineHeight: 'normal',
-        letterSpacing: 0.3,
-        color: '#757575',
-      },
-    }
-  },
-  inputFocused: {
-    border: 'none',
-  },
-  suggestionHighlighted: {
-    backgroundColor: '#552e89',
-    opacity: 1,
-  },
-  suggestionDivider: {
-    marginTop: 5,
-    '@media (max-width: 1023px)': {
-      marginTop: 20,
-      marginLeft: 16,
-      marginRight: 16,
-    },
-  },
-  suggestionContainer: {
-    backgroundColor: 'white',
-    opacity: 1,
-    zIndex: 10,
-    height: 80,
-    boxSizing: 'border-box',
-    paddingLeft: 10,
-    '&:hover': {
-      backgroundColor: '#552e89',
-      color: 'white',
-      opacity: 1,
-    },
-    '@media (max-width: 1023px)': {
-      height: 50,
-      marginLeft: 16,
-      width: 'calc(100vw - 32px)',
-      marginRight: 16,
-      paddingLeft: 16,
-      '&:hover': {
-        backgroundColor: '#552e89',
-        color: 'white',
-        opacity: 1,
-        paddingLeft: 16,
-      },
-    }
-  },
-  suggestionItem: {
-    fontSize: 24,
-    height: 35,
-    paddingLeft: 'calc(30px + 0.5%)',
-    display: 'flex',
-    alignItems: 'center',
-    '@media (max-width: 1023px)': {
-      height: 35,
-      paddingLeft: 0,
-    }
   },
   container: {
     height: '100%',
@@ -141,31 +52,13 @@ class SearchForm extends React.Component {
     searchInput.focus();
   }
 
-  getSuggestionValue = suggestion => suggestion
+  getSuggestionValue = suggestion => suggestion.name
 
-  renderSuggestion = (suggestion) => {
-    const { classes } = this.props;
-    return (
-      <MenuItem component="div" className={classes.suggestionContainer}>
-        <div className={classes.suggestionItem}>
-          {suggestion.name}
-        </div>
-      </MenuItem>
-    );
-  }
-
-  renderSuggestionsContainer = (options) => {
-    const { classes } = this.props;
-    const { containerProps, children } = options;
-    const divider = <div className={classes.suggestionDivider} />;
-
-    return (
-      <div {...containerProps}>
-        {children !== null ? divider : null}
-        {children}
-      </div>
-    );
-  }
+  renderSuggestion = suggestion => (
+    <span>
+      {suggestion.name}
+    </span>
+  )
 
   onSuggestionSelected = (event, { suggestion }) => {
     const { cancelSearch, history } = this.props;
@@ -190,7 +83,7 @@ class SearchForm extends React.Component {
     const { searchListArtists } = this.props;
     this.timeout = setTimeout(() => {
       searchListArtists(value);
-    }, 450);
+    }, 300);
   };
 
   onSuggestionsClearRequested = () => {
@@ -209,7 +102,9 @@ class SearchForm extends React.Component {
     const inputProps = {
       placeholder: 'Search artists',
       value,
-      style: {},
+      style: {
+        backgroundColor: 'white'
+      },
       onChange: this.onChange,
       id: 'search-input',
     };
@@ -218,14 +113,12 @@ class SearchForm extends React.Component {
         <div>
           <Autosuggest
             id="search-bar"
-            theme={classes}
             suggestions={suggestions}
             onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
             onSuggestionsClearRequested={this.onSuggestionsClearRequested}
             onSuggestionSelected={this.onSuggestionSelected}
             getSuggestionValue={this.getSuggestionValue}
             renderSuggestion={this.renderSuggestion}
-            renderSuggestionsContainer={this.renderSuggestionsContainer}
             inputProps={inputProps}
           />
           <CloseIcon className={classes.closeIcon} onClick={this.onCloseClick} />
@@ -245,3 +138,12 @@ const mapActionsToProps = dispatch => bindActionCreators({
 }, dispatch);
 
 export const SearchFormConnect = withStyles(styles)(withRouter(connect(mapStateToProps, mapActionsToProps)(SearchForm)));
+
+SearchForm.propTypes = {
+  suggestions: arrayOf(any).isRequired,
+  classes: objectOf(any).isRequired,
+  clearListArtists: func.isRequired,
+  searchListArtists: func.isRequired,
+  cancelSearch: func.isRequired,
+  history: objectOf(any).isRequired
+};
